5b5cdecce3c4eba26e1f38b7dd6a2c792a44a6f9
Kevin Booth <kdbooth97@gmail.com>
2018-11-21 00:17:08 -0500

    Add massage_api_response() function

--- api.py
+++ api.py
@@ -5,13 +5,15 @@ Created by Kevin Booth
 Nov 15, 2017
 """
 
-import googlemaps
+from bs4 import BeautifulSoup
+from collections import defaultdict
 from datetime import datetime
+import googlemaps
 import json
 
 
 
-class GoogleMaps:
+class GoogleMapsHelper:
     """
     Provides functions to use the Google Maps API
     """
@@ -26,10 +28,28 @@ class GoogleMaps:
         gmaps = googlemaps.Client(key='AIzaSyCtjrtull-lGC4jo0IwlSJBTAf8GhZkuSY')
 
         now = datetime.now()
-        directions_result = gmaps.directions(travel_information['start_address'],
-                                     travel_information['end_address'],
-                                     mode=travel_information['travel_mode'],
-                                     departure_time=now)
+        #directions_result = gmaps.directions(travel_information['start_address'],
+        #                             travel_information['end_address'],
+        #                             mode=travel_information['travel_mode'],
+        #                             departure_time=now)
 
-        #direction_data = json.loads(str(directions_result))
-        print(json.dumps(directions_result))
+        directions_result = gmaps.directions('1 S Main St, Manchester NH 03102',
+                                     '175 Canal St, Manchester NH 03101',
+                                     mode='driving',
+                                     departure_time=now)
+        directions_result = json.dumps(directions_result)
+        return json.loads(directions_result)[0]
+
+    def massage_api_response(api_data):
+        """
+        Massages and manipulates the data retrieved from Google Maps to
+        prepare it for PDF writing
+        Returns: dictionary of strings and lists
+        """
+        return_dict = defaultdict(list)
+        for instruction in api_data['legs'][0]['steps']:
+            return_dict['instructions']
+            .append(BeautifulSoup(instruction['html_instructions']).get_text())
+        print(api_data['legs'][0]['steps'][0]['html_instructions'])
+        print(' ')
+        print(return_dict['instructions'])
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Created by Kevin Booth
 Nov 15, 2017
 """
 
-from api import GoogleMaps
+from api import GoogleMapsHelper
 from user import UserInterface
 import googlemaps
 from datetime import datetime
@@ -15,4 +15,5 @@ from datetime import datetime
 if __name__ == '__main__':
     UserInterface.welcome_message()
     user_data = UserInterface.user_data_retrieval()
-    GoogleMaps.api_call(user_data)
+    api_call = GoogleMapsHelper.api_call(user_data)
+    api_data = GoogleMapsHelper.massage_api_response(api_call)
