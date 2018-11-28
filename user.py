87de449f7142c080b7d6d40a20fe4feb5a3d35b6
AdamHarney <adamharney0@gmail.com>
2018-11-28 15:48:19 -0500

    implemented user error checkers in user.py

--- user.py
+++ user.py
@@ -25,6 +25,8 @@ class UserInterface:
             ending destination
             mode of transportation
         """
+
+        tMode = {'walking':1, 'bicycling':2, 'transit':3, 'driving':4}
         start_street = input('Please enter the starting street address (ex: 1 Main St): ')
         start_city = input('Please enter the starting city (ex: Manchester): ')
         start_state = input('Please enter the starting state (ex: NH): ')
@@ -40,7 +42,27 @@ class UserInterface:
         end_address = str(end_street) + ', ' + str(end_city) + ' ' + str(end_state) + ' ' + str(end_zipcode)
 
         travel_mode = input('Please choose from the following travel modes: walking, bicycling, transit, or driving: ')
-        depart_now = input('Would you like to depart now? [Y/n]: ')
+        correctbool=False
+        while correctbool == False:
+            err = 0
+            for a in tMode:
+                if travel_mode != a:
+                    err+=1
+                    if err >= 4:
+                        print('The mode of travel you have entered does not match any of the options.')
+                        travel_mode = input('Please enter one the following travel modes: WALKING, BICYCLING, TRANSIT, or DRIVING: ')
+
+                else:
+                    correctbool = True
+
+        depart_now = input('Would you like to depart now? [y/n]: ')
+        correctbool = False
+        while correctbool == False:
+            if depart_now != 'y':
+                print('Invalid response! Please enter \'y\' for \'yes\' or \'n\' for \'no.\'\n')
+                depart_now = input('Would you like to depart now? [y/n]: ')
+            else:
+                correctbool = True
 
         travel_information = {'start_address': start_address, 'end_address': end_address, 'travel_mode': travel_mode, 'depart_now': depart_now}
 
